7e5adbc6384d35e84a3d8da341da6901228a188f
Slavi Palahanov <slavcho.palahanov@gmail.com>
2024-10-31 21:09:49 +0200

    Remove and sort namespaces

--- DentalManagementSystem.Data.Models/Appointment.cs
+++ DentalManagementSystem.Data.Models/Appointment.cs
@@ -1,13 +1,9 @@
 ﻿namespace DentalManagementSystem.Data.Models
 {
+    using DentalManagementSystem.Common.Enums;
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations.Schema;
     using System.ComponentModel.DataAnnotations;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using DentalManagementSystem.Common.Enums;
 
     public class Appointment
     {
