64e4e99cf4234db25ca2196af395643fd9ba540e
Slavi Palahanov <slavcho.palahanov@gmail.com>
2024-10-31 18:26:06 +0200

    Create new entity AppointmentType and and define validation constants

--- DentalManagementSystem.Common/Constants/EntityValidationConstants.cs
+++ DentalManagementSystem.Common/Constants/EntityValidationConstants.cs
@@ -16,5 +16,12 @@ namespace DentalManagementSystem.Common.Constants
             public const int DescriptionMinLength = 50;
             public const int DescriptionMaxLength = 500;
         }
+
+        public class AppointmentType
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 30;
+        }
+
     }
 }
---
+++ DentalManagementSystem.Data.Models/AppointmentType.cs
@@ -0,0 +1,24 @@
+﻿namespace DentalManagementSystem.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using static DentalManagementSystem.Common.Constants.EntityValidationConstants.AppointmentType;
+
+    public class AppointmentType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual IEnumerable<Appointment> Appointments { get; set; } =
+            new HashSet<Appointment>();
+    }
+}
