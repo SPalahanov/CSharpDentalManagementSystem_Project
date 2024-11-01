0e9e972ba607ae924b17fff09eefe52718a2eeda
Slavi Palahanov <slavcho.palahanov@gmail.com>
2024-11-01 18:03:26 +0200

    Implement AppointmentType configuration and seeding

---
+++ DentalManagementSystem.Data/Configurations/AppointmentTypeEntityConfiguration.cs
@@ -0,0 +1,53 @@
+﻿namespace DentalManagementSystem.Data.Configurations
+{
+    using DentalManagementSystem.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class AppointmentTypeEntityConfiguration : IEntityTypeConfiguration<AppointmentType>
+    {
+        public void Configure(EntityTypeBuilder<AppointmentType> builder)
+        {
+            builder.HasData(this.GenerateAppointmentType());
+        }
+
+        private AppointmentType[] GenerateAppointmentType()
+        {
+            ICollection<AppointmentType> appointmentTypes = new HashSet <AppointmentType>();
+
+            AppointmentType appointmentType;
+
+            appointmentType = new AppointmentType() 
+            { 
+                Id = 1,
+                Name = "Consultation"
+            };
+            appointmentTypes.Add(appointmentType);
+
+            appointmentType = new AppointmentType()
+            {
+                Id = 2,
+                Name = "Examination"
+            };
+            appointmentTypes.Add(appointmentType);
+
+            appointmentType = new AppointmentType()
+            {
+                Id = 3,
+                Name = "Routine care"
+            };
+            appointmentTypes.Add(appointmentType);
+
+            appointmentType = new AppointmentType()
+            {
+                Id = 4,
+                Name = "Surgery"
+            };
+            appointmentTypes.Add(appointmentType);
+
+            return appointmentTypes.ToArray();
+        }
+    }
+}
