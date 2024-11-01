4eb638a5fd3635d91e3a0f8b03f7b89cda317991
Slavi Palahanov <slavcho.palahanov@gmail.com>
2024-11-01 05:59:00 +0200

    Implement configuration for Appointment entity

---
+++ DentalManagementSystem.Data/Configurations/AppointmentEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace DentalManagementSystem.Data.Configurations
+{
+    using DentalManagementSystem.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AppointmentEntityConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            builder
+                .HasOne(a => a.AppointmentType)
+                .WithMany(at => at.Appointments)
+                .HasForeignKey(a => a.AppointmentTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
