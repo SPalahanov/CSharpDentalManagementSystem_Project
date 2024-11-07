128b9ff9fe4d3a5ef8db35791c649c5236482057
Slavi Palahanov <slavcho.palahanov@gmail.com>
2024-11-07 19:16:37 +0200

    Add Irepository and BaseRepository for data access abstraction

---
+++ DentalManagementSystem.Data/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace DentalManagementSystem.Data.Repository.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IRepository<TType, TId>
+    {
+        TType GetById(TId id);
+
+        Task<TType> GetByIdAsync(TId id);
+
+        Task<TType> GetByIdAsync(params TId[] ids);
+
+        IEnumerable<TType> GetAll();
+
+        Task<IEnumerable<TType>> GetAllAsync();
+
+        IQueryable<TType> GetAllAttached();
+
+        void Add(TType item);
+
+        Task AddAsync(TType item);
+
+        void AddRange(TType[] items);
+
+        Task AddRangeAsync(TType[] items);
+
+        bool Delete(TId id);
+
+        Task<bool> DeleteAsync(TId id);
+
+        bool Update(TType item);
+
+        Task<bool> UpdateAsync(TType item);
+    }
+}
