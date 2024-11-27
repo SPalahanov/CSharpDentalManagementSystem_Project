54f0413918f3a2e81897ce075ff9298ea32cd154
Slavi Palahanov <slavcho.palahanov@gmail.com>
2024-11-27 17:08:50 +0200

    Added Admin area with HomeController and views

---
+++ DentalManagementSystem.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace DentalManagementSystem.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static DentalManagementSystem.Common.Constants.GeneralApplicationConstants;
+
+    [Area(AdminRoleName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
